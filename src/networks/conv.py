0aca9db2f0008095fb58819679b97f5fbd23da50
Quan Dou <qd9zb@virginia.edu>
2023-07-13 01:30:50 -0400

    rewrite resnet model

---
+++ src/networks/conv.py
@@ -0,0 +1,194 @@
+from torch import nn
+import ctorch as cnn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, dimensions, real, in_channels,
+                 out_channels, kernel_size=3, bias=True,
+                 normalization=None, activation=None):
+        super().__init__()
+        if dimensions == 1:
+            conv = nn.Conv1d if real else cnn.CConv1d
+        elif dimensions == 2:
+            conv = nn.Conv2d if real else cnn.CConv2d
+        elif dimensions == 3:
+            conv = nn.Conv3d if real else cnn.CConv3d
+        else:
+            raise ValueError('dimensions should be 1 or 2 or 3.')
+        layers = [conv(in_channels, out_channels, kernel_size,
+                       padding='same', bias=bias)]
+        if normalization is not None:
+            if normalization == 'batch':
+                if dimensions == 1:
+                    norm = nn.BatchNorm1d if real else cnn.CBatchNorm1d
+                elif dimensions == 2:
+                    norm = nn.BatchNorm2d if real else cnn.CBatchNorm2d
+                else:
+                    norm = nn.BatchNorm3d if real else cnn.CBatchNorm3d
+            elif normalization == 'instance':
+                if dimensions == 1:
+                    norm = nn.InstanceNorm1d if real else cnn.CInstanceNorm1d
+                elif dimensions == 2:
+                    norm = nn.InstanceNorm2d if real else cnn.CInstanceNorm2d
+                else:
+                    norm = nn.InstanceNorm3d if real else cnn.CInstanceNorm3d
+            else:
+                raise ValueError('normalization should be None '
+                                 'or batch or instance.')
+            layers.append(norm(out_channels))
+        if activation is not None:
+            if activation == 'ReLU':
+                layers.append(nn.ReLU() if real else cnn.CReLU())
+            elif activation == 'LeakyReLU':
+                layers.append(nn.LeakyReLU() if real else cnn.CLeakyReLU())
+            else:
+                raise ValueError('activation should be None or '
+                                 'ReLU or LeakyReLU.')
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.block(input)
+
+
+class ConvBlock1d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(1, True, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class ConvBlock2d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(2, True, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class ConvBlock3d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(3, True, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class ConvBlock2plus1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__()
+        mid_channels = int((kernel_size ** 3 * in_channels * out_channels) /
+                           (kernel_size ** 2 * in_channels +
+                            kernel_size * out_channels))
+        layers = [nn.Conv3d(in_channels, mid_channels,
+                            (1, kernel_size, kernel_size),
+                            padding='same', bias=bias)]
+        if normalization is not None:
+            if normalization == 'batch':
+                layers.append(nn.BatchNorm3d(mid_channels))
+            elif normalization == 'instance':
+                layers.append(nn.InstanceNorm3d(mid_channels))
+            else:
+                raise ValueError('normalization should be None '
+                                 'or batch or instance.')
+        if activation is not None:
+            if activation == 'ReLU':
+                layers.append(nn.ReLU())
+            elif activation == 'LeakyReLU':
+                layers.append(nn.LeakyReLU())
+            else:
+                raise ValueError('activation should be None or '
+                                 'ReLU or LeakyReLU.')
+        layers.append(nn.Conv3d(mid_channels, out_channels,
+                                (kernel_size, 1, 1),
+                                padding='same', bias=bias))
+        if normalization is not None:
+            if normalization == 'batch':
+                layers.append(nn.BatchNorm3d(mid_channels))
+            elif normalization == 'instance':
+                layers.append(nn.InstanceNorm3d(mid_channels))
+            else:
+                raise ValueError('normalization should be None '
+                                 'or batch or instance.')
+        if activation is not None:
+            if activation == 'ReLU':
+                layers.append(nn.ReLU())
+            elif activation == 'LeakyReLU':
+                layers.append(nn.LeakyReLU())
+            else:
+                raise ValueError('activation should be None or '
+                                 'ReLU or LeakyReLU.')
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.block(input)
+
+
+class CConvBlock1d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(1, False, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class CConvBlock2d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(2, False, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class CConvBlock3d(ConvBlock):
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 bias=True, normalization=None, activation=None):
+        super().__init__(3, False, in_channels, out_channels, kernel_size,
+                         bias, normalization, activation)
+
+
+class CConvBlock2plus1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
+                 normalization=None, activation=None):
+        super().__init__()
+        mid_channels = int((kernel_size ** 3 * in_channels * out_channels) /
+                           (kernel_size ** 2 * in_channels +
+                            kernel_size * out_channels))
+        layers = [cnn.CConv3d(in_channels, mid_channels,
+                              (1, kernel_size, kernel_size),
+                              padding='same', bias=bias)]
+        if normalization is not None:
+            if normalization == 'batch':
+                layers.append(cnn.CBatchNorm3d(mid_channels))
+            elif normalization == 'instance':
+                layers.append(cnn.CInstanceNorm3d(mid_channels))
+            else:
+                raise ValueError('normalization should be None '
+                                 'or batch or instance.')
+        if activation is not None:
+            if activation == 'ReLU':
+                layers.append(cnn.CReLU())
+            elif activation == 'LeakyReLU':
+                layers.append(cnn.CLeakyReLU())
+            else:
+                raise ValueError('activation should be None or '
+                                 'ReLU or LeakyReLU.')
+        layers.append(cnn.CConv3d(mid_channels, out_channels,
+                                  (kernel_size, 1, 1),
+                                  padding='same', bias=bias))
+        if normalization is not None:
+            if normalization == 'batch':
+                layers.append(cnn.CBatchNorm3d(mid_channels))
+            elif normalization == 'instance':
+                layers.append(cnn.CInstanceNorm3d(mid_channels))
+            else:
+                raise ValueError('normalization should be None '
+                                 'or batch or instance.')
+        if activation is not None:
+            if activation == 'ReLU':
+                layers.append(cnn.CReLU())
+            elif activation == 'LeakyReLU':
+                layers.append(cnn.CLeakyReLU())
+            else:
+                raise ValueError('activation should be None or '
+                                 'ReLU or LeakyReLU.')
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.block(input)
--- src/networks/resnet.py
+++ src/networks/resnet.py
@@ -1,275 +1,83 @@
-import torch
-from torch import nn
-import ctorch as cnn
+from conv import *
 
 
-class ConvBlock2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
+class ResNet(nn.Module):
+    def __init__(self, dimensions, real, in_channels, out_channels,
+                 depth, num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
         super().__init__()
-        layers = [nn.Conv2d(in_channels, out_channels, kernel_size,
-                            padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(nn.BatchNorm2d(out_channels))
-            elif normalization == 'instance':
-                layers.append(nn.InstanceNorm2d(out_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(nn.ReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(nn.LeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class ConvBlock3d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
-        super().__init__()
-        layers = [nn.Conv3d(in_channels, out_channels, kernel_size,
-                            padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(nn.BatchNorm3d(out_channels))
-            elif normalization == 'instance':
-                layers.append(nn.InstanceNorm3d(out_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(nn.ReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(nn.LeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class ConvBlock2plus1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
-        super().__init__()
-        mid_channels = int((kernel_size ** 3 * in_channels * out_channels) /
-                           (kernel_size ** 2 * in_channels +
-                            kernel_size * out_channels))
-        layers = [nn.Conv3d(in_channels, mid_channels,
-                            (1, kernel_size, kernel_size),
-                            padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(nn.BatchNorm3d(mid_channels))
-            elif normalization == 'instance':
-                layers.append(nn.InstanceNorm3d(mid_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(nn.ReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(nn.LeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        layers.append(nn.Conv3d(mid_channels, out_channels,
-                                (kernel_size, 1, 1),
-                                padding='same', bias=bias))
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(nn.BatchNorm3d(mid_channels))
-            elif normalization == 'instance':
-                layers.append(nn.InstanceNorm3d(mid_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(nn.ReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(nn.LeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class CConvBlock2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
-        super().__init__()
-        layers = [cnn.CConv2d(in_channels, out_channels, kernel_size,
-                              padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(cnn.CBatchNorm2d(out_channels))
-            elif normalization == 'instance':
-                layers.append(cnn.CInstanceNorm2d(out_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(cnn.CReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(cnn.CLeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class CConvBlock3d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
-        super().__init__()
-        layers = [cnn.CConv3d(in_channels, out_channels, kernel_size,
-                              padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(cnn.CBatchNorm3d(out_channels))
-            elif normalization == 'instance':
-                layers.append(cnn.CInstanceNorm3d(out_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(cnn.CReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(cnn.CLeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class CConvBlock2plus1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, bias=True,
-                 normalization=None, activation=None):
-        super().__init__()
-        mid_channels = int((kernel_size ** 3 * in_channels * out_channels) /
-                           (kernel_size ** 2 * in_channels +
-                            kernel_size * out_channels))
-        layers = [cnn.CConv3d(in_channels, mid_channels,
-                              (1, kernel_size, kernel_size),
-                              padding='same', bias=bias)]
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(cnn.CBatchNorm3d(mid_channels))
-            elif normalization == 'instance':
-                layers.append(cnn.CInstanceNorm3d(mid_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(cnn.CReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(cnn.CLeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        layers.append(cnn.CConv3d(mid_channels, out_channels,
-                                  (kernel_size, 1, 1),
-                                  padding='same', bias=bias))
-        if normalization is not None:
-            if normalization == 'batch':
-                layers.append(cnn.CBatchNorm3d(mid_channels))
-            elif normalization == 'instance':
-                layers.append(cnn.CInstanceNorm3d(mid_channels))
-            else:
-                raise ValueError('normalization should be None '
-                                 'or batch or instance.')
-        if activation is not None:
-            if activation == 'ReLU':
-                layers.append(cnn.CReLU())
-            elif activation == 'LeakyReLU':
-                layers.append(cnn.CLeakyReLU())
-            else:
-                raise ValueError('activation should be None or '
-                                 'ReLU or LeakyReLU.')
-        self.block = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.block(input)
-
-
-class ResNet2d(nn.Module):
-    def __init__(self, in_channels, out_channels, depth, num_filters=64,
-                 kernel_size=3, normalization=None, activation='ReLU'):
-        super().__init__()
-        blocks = [ConvBlock2d(in_channels, num_filters, kernel_size,
-                              normalization, activation)]
         if depth < 2:
             raise ValueError('depth should be greater than 2.')
+        if dimensions == '2':
+            conv_block = ConvBlock2d if real else CConvBlock2d
+        elif dimensions == '3':
+            conv_block = ConvBlock3d if real else CConvBlock3d
+        elif dimensions == '2+1':
+            conv_block = ConvBlock2plus1d if real else CConvBlock2plus1d
+        else:
+            raise ValueError('dimensions should be 2 or 3 or 2+1.')
+        blocks = [conv_block(in_channels, num_filters, kernel_size,
+                             bias, normalization, activation)]
         for _ in range(depth - 2):
-            blocks.append(ConvBlock2d(num_filters, num_filters, kernel_size,
-                                      normalization, activation))
-        blocks.append(ConvBlock2d(num_filters, out_channels, kernel_size,
-                                  normalization, activation))
+            blocks.append(conv_block(num_filters, num_filters, kernel_size,
+                                     bias, normalization, activation))
+        blocks.append(conv_block(num_filters, out_channels, kernel_size,
+                                 bias, normalization, activation))
         self.net = nn.Sequential(*blocks)
 
     def forward(self, input):
         return self.net(input) + input
 
 
-class ResNet3d(nn.Module):
-    def __init__(self, in_channels, out_channels, depth, num_filters=64,
-                 kernel_size=3, normalization=None, activation='ReLU'):
-        super().__init__()
-        blocks = [ConvBlock3d(in_channels, num_filters, kernel_size,
-                              normalization, activation)]
-        if depth < 2:
-            raise ValueError('depth should be greater than 2.')
-        for _ in range(depth - 2):
-            blocks.append(ConvBlock3d(num_filters, num_filters, kernel_size,
-                                      normalization, activation))
-        blocks.append(ConvBlock3d(num_filters, out_channels, kernel_size,
-                                  normalization, activation))
-        self.net = nn.Sequential(*blocks)
-
-    def forward(self, input):
-        return self.net(input) + input
+class ResNet2d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('2', True, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
 
 
-class ResNet2plus1d(nn.Module):
-    def __init__(self, in_channels, out_channels, depth, num_filters=64,
-                 kernel_size=3, normalization=None, activation='ReLU'):
-        super().__init__()
-        blocks = [ConvBlock2plus1d(in_channels, num_filters, kernel_size,
-                                   normalization, activation)]
-        if depth < 2:
-            raise ValueError('depth should be greater than 2.')
-        for _ in range(depth - 2):
-            blocks.append(ConvBlock2plus1d(num_filters, num_filters,
-                                           kernel_size, normalization,
-                                           activation))
-        blocks.append(ConvBlock2plus1d(num_filters, out_channels, kernel_size,
-                                       normalization, activation))
-        self.net = nn.Sequential(*blocks)
+class ResNet3d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('3', True, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
 
-    def forward(self, input):
-        return self.net(input) + input
+
+class ResNet2plus1d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('2+1', True, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
+
+
+class CResNet2d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=32, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('2', False, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
+
+
+class CResNet3d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('3', False, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
+
+
+class CResNet2plus1d(ResNet):
+    def __init__(self, in_channels, out_channels, depth,
+                 num_filters=64, kernel_size=3, bias=True,
+                 normalization=None, activation='ReLU'):
+        super().__init__('2+1', False, in_channels, out_channels,
+                         depth, num_filters, kernel_size, bias,
+                         normalization, activation)
