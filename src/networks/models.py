b4c022b4dfdc50a1d1be7219683a0d01592f2cc5
Quan Dou <qd9zb@virginia.edu>
2023-08-16 09:42:37 -0400

    add cascade model

---
+++ src/networks/models.py
@@ -0,0 +1,69 @@
+from .dc import *
+from .unet import *
+
+
+class SingleCoilCascadeCrossDomainNet(Module):
+    def __init__(self, in_channels, out_channels, cascade_depth=2,
+                 net_depth=4, num_filters=64, kernel_size=3, bias=False,
+                 normalization='instance', activation='relu', down='avg',
+                 up='bilinear', lamda=0.99, channel_dim=1):
+        super().__init__()
+        self.knets = ModuleList([UNet(
+            in_channels, out_channels, net_depth, num_filters, kernel_size,
+            bias, normalization, activation, down, up)] * cascade_depth)
+        self.inets = ModuleList([UNet(
+            in_channels, out_channels, net_depth, num_filters, kernel_size,
+            bias, normalization, activation, down, up)] * cascade_depth)
+        self.real_complex = RealComplex(channel_dim)
+        self.complex_real = ComplexReal(channel_dim)
+        self.fft2c = FFT2C()
+        self.ifft2c = IFFT2C()
+        self.data_consistency = DataConsistency(lamda)
+
+    def forward(self, ksub, mask):
+        kcnn = ksub
+        for knet, inet in zip(self.knets, self.inets):
+            kcnn = self.real_complex(knet(self.complex_real(kcnn)))
+            imcnn = self.ifft2c(kcnn)
+            imcnn = self.real_complex(inet(self.complex_real(imcnn)))
+            kcnn = self.fft2c(imcnn)
+            kcnn = self.data_consistency(kcnn, mask, ksub)
+        imcnn = self.ifft2c(kcnn)
+        return torch.abs(imcnn)
+
+
+class MultiCoilCascadeCrossDomainNet(Module):
+    def __init__(self, in_channels, out_channels, cascade_depth=2,
+                 net_depth=4, num_filters=64, kernel_size=3, bias=False,
+                 normalization='instance', activation='relu', down='avg',
+                 up='bilinear', lamda=0.99, channel_dim=1, coil_dim=2):
+        super().__init__()
+        self.knets = ModuleList([UNet(
+            in_channels, out_channels, net_depth, num_filters, kernel_size,
+            bias, normalization, activation, down, up)] * cascade_depth)
+        self.inets = ModuleList([UNet(
+            in_channels, out_channels, net_depth, num_filters, kernel_size,
+            bias, normalization, activation, down, up)] * cascade_depth)
+        self.real_complex = RealComplex(channel_dim)
+        self.complex_real = ComplexReal(channel_dim)
+        self.fft2c = FFT2C()
+        self.ifft2c = IFFT2C()
+        self.coil_split = CoilSplit(coil_dim)
+        self.coil_combine = CoilCombine(coil_dim)
+        self.data_consistency = DataConsistency(lamda)
+        self.coil_dim = coil_dim
+
+    def forward(self, ksub, mask, sens):
+        kcnn = ksub
+        for knet, inet in zip(self.knets, self.inets):
+            stack = []
+            for i in range(kcnn.shape[self.coil_dim]):
+                stack.append(self.real_complex(knet(self.complex_real(
+                    kcnn.select(self.coil_dim, i)))))
+            kcnn = torch.stack(stack, self.coil_dim)
+            imcnn = self.coil_combine(self.ifft2c(kcnn), sens)
+            imcnn = self.real_complex(inet(self.complex_real(imcnn)))
+            kcnn = self.coil_split(self.fft2c(imcnn), sens)
+            kcnn = self.data_consistency(kcnn, mask, ksub)
+        imcnn = self.coil_combine(self.ifft2c(kcnn), sens)
+        return torch.abs(imcnn)
