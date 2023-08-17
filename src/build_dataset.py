6de9102ad770ff0dc0c9c2fe0c473ba0d6cf3d50
Quan Dou <qd9zb@virginia.edu>
2023-08-17 15:45:42 -0400

    modify build_dataset script to support training-validation split

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -12,51 +12,60 @@ def get_parser():
                         help='Root path of the raw data.')
     parser.add_argument('--new_root', type=str,
                         help='Root path of the new dataset.')
-    parser.add_argument('--multi_coil', default=False, action='store_true',
-                        help='Single coil or multi coil.')
+    parser.add_argument('--trn_ratio', type=float, default=0.85,
+                        help='Training ratio, validation ratio = '
+                             '1 - training ratio.')
     return parser
 
 
-def build_dataset(old_root, new_root, multi_coil):
-    if not os.path.exists(new_root):
-        os.makedirs(new_root)
-    for subject in os.listdir(old_root):
-        if os.path.isdir(os.path.join(old_root, subject)):
-            print('Start processing subject {} ...'.format(subject))
-            start_time = time.perf_counter()
-            for data_file in ['cine_lax', 'cine_sax']:
-                old_filename = os.path.join(
-                    old_root, subject, data_file + '.mat')
-                if os.path.exists(old_filename):
-                    kfull = h5read(old_filename, 'kspace_full' if multi_coil
+def build_dataset(old_root, new_root, multi_coil, trn_ratio):
+    trn_root = os.path.join(new_root, 'TrainingSet')
+    val_root = os.path.join(new_root, 'ValidationSet')
+    if not os.path.exists(trn_root):
+        os.makedirs(trn_root)
+    if not os.path.exists(val_root):
+        os.makedirs(val_root)
+    subjects = [subject for subject in os.listdir(old_root)
+                if os.path.isdir(os.path.join(old_root, subject))]
+    trn_num = int(trn_ratio * len(subjects))
+    subjects.sort()
+    rng = np.random.default_rng(seed=42)
+    rng.shuffle(subjects)
+    for i, subject in enumerate(subjects):
+        print('Start processing subject {} ...'.format(subject))
+        start_time = time.perf_counter()
+        for data_file in ['cine_lax', 'cine_sax']:
+            old_filename = os.path.join(
+                old_root, subject, data_file + '.mat')
+            if os.path.exists(old_filename):
+                kfull = h5read(old_filename, 'kspace_full' if multi_coil
                                    else 'kspace_single_full')
-                    kfull = kfull['real'] + 1j * kfull['imag']
-                    for i in range(kfull.shape[1]):
-                        kfull[:, i] /= np.max(np.abs(kfull[:, i]))
-                    for i in range(kfull.shape[1]):
+                kfull = kfull['real'] + 1j * kfull['imag']
+                for s in range(kfull.shape[1]):
+                    kfull[:, s] /= np.max(np.abs(kfull[:, s]))
+                    if multi_coil:
+                        sens = espirit_map(np.mean(kfull[:, s], axis=0))
+                    for f in range(kfull.shape[0]):
+                        new_filename = os.path.join(
+                            trn_root if i < trn_num else val_root,
+                            '{}_{}_frame{:02d}_slice{:02d}.h5'.format(
+                                subject, data_file, f, s))
+                        h5write(new_filename, 'kspace',
+                                kfull[f][s].astype(np.csingle))
                         if multi_coil:
-                            sens = espirit_map(np.mean(kfull[:, i], axis=0))
-                        else:
-                            sens = None
-                        for j in range(kfull.shape[0]):
-                            new_filename = os.path.join(
-                                new_root, f'{subject}_{data_file}_'
-                                          f'frame{j:02d}_slice{i:02d}.h5')
-                            h5write(new_filename, 'kspace',
-                                    kfull[j][i].astype(np.csingle))
-                            if sens:
-                                h5write(new_filename, 'sensitivity_map',
+                            h5write(new_filename, 'sensitivity_map',
                                     sens.astype(np.csingle))
-            print('Finish processing subject {}: {:.3f} seconds.'.format(
-                subject, time.perf_counter() - start_time))
+        print('Finish processing subject {}: {:.3f} seconds.'.format(
+            subject, time.perf_counter() - start_time))
 
 
 def main():
     args = get_parser().parse_args()
     old_root = args.old_root
     new_root = args.new_root
-    multi_coil = args.multi_coil
-    build_dataset(old_root, new_root, multi_coil)
+    multi_coil = 'MultiCoil' in old_root
+    trn_ratio = args.trn_ratio
+    build_dataset(old_root, new_root, multi_coil, trn_ratio)
 
 
 if __name__ == '__main__':
